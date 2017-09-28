fda862ed6d04fd6541ef6bc65df1d27da1d3cb8c
William Kenji Kiplinger <wkkiplinger@cpp.edu>
2017-09-28 11:58:58 -0700

    update 1
    client:
    -sends messages to server
    -reads messages sent from server
    server:
    -reads messages from client
    -returns messages to server
    -server terminal writes when someone connects and disconnects

--- EX1/EchoClient.java
+++ EX1/EchoClient.java
@@ -1,32 +1,58 @@
 
 import java.io.InputStream;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.net.Socket;
+import java.util.Random;
+import java.util.Scanner;
 
 public final class EchoClient {
 
-    public static void main(String[] args) throws Exception {
-        try (Socket socket = new Socket("localhost", 22222)) {
-            InputStream is = socket.getInputStream();
-            InputStreamReader isr = new InputStreamReader(is, "UTF-8");
-            BufferedReader br = new BufferedReader(isr);
-            System.out.println(br.readLine());
-        }
-    }
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		Socket socket = new Socket("localhost", 22222);
+
+		InputStream is = socket.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+		BufferedReader br = new BufferedReader(isr);
+
+		Runnable read = () -> {
+			String line = "";
+			while (true) {
+				try {
+					line = br.readLine();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+				if (line != null) {
+					System.out.println(line);
+				}
+			}
+		};
+
+		String address = socket.getInetAddress().getHostAddress();
+		OutputStream os = socket.getOutputStream();
+		PrintStream out = new PrintStream(os, true, "UTF-8");
+
+		Runnable write = () -> {
+			while (true) {
+				try {
+					Thread.sleep(5);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.print("Client> ");
+				out.printf("%s%n", sc.nextLine());
+			}
+
+		};
+
+		Thread readThread = new Thread(read);
+		Thread writeThread = new Thread(write);
+		readThread.start();
+		writeThread.start();
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- EX1/EchoServer.java
+++ EX1/EchoServer.java
@@ -1,22 +1,51 @@
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Random;
 
 public final class EchoServer {
 
-    public static void main(String[] args) throws Exception {
-        try (ServerSocket serverSocket = new ServerSocket(22222)) {
-            while (true) {
-                try (Socket socket = serverSocket.accept()) {
-                    String address = socket.getInetAddress().getHostAddress();
-                    System.out.printf("Client connected: %s%n", address);
-                    OutputStream os = socket.getOutputStream();
-                    PrintStream out = new PrintStream(os, true, "UTF-8");
-                    out.printf("Hi %s, thanks for connecting!%n", address);
-                }
-            }
-        }
-    }
+	public static void main(String[] args) throws Exception {
+
+		Runnable read = () -> {
+			try (ServerSocket serverSocket = new ServerSocket(22222)) {
+				while (true) {
+					try (Socket socket = serverSocket.accept()) {
+
+						InputStream is = socket.getInputStream();
+						InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+						BufferedReader br = new BufferedReader(isr);
+
+						String address = socket.getInetAddress().getHostAddress();
+						System.out.printf("Client connected: %s%n", address);
+						OutputStream os = socket.getOutputStream();
+						PrintStream out = new PrintStream(os, true, "UTF-8");
+						out.printf("Hi %s, thanks for connecting!%n", address);
+						try {
+							while(true){
+								out.printf("Server> %s%n", br.readLine());
+							}
+						} catch (IOException e) {
+							System.out.printf("Client disconnected: %s%n", address);
+						}
+
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		};
+
+		Thread readThread = new Thread(read);
+		readThread.start();
+
+	}
 }
